07bbe9abffc5849e467d2af7895dcf51f007caf3
Eduardo <eduardo.rondon@apstem.com>
2020-05-19 22:09:47 +0200

    feat: project release for review

--- README.md
+++ README.md
@@ -4,13 +4,26 @@
 
 * [Strategy](#Strategy)
 * [Creation Navbar](#Navbar)
+* [Intersection Observer](#Observer)
+* [Event Listener](#ClickEvent)
 
 ## Strategy
 
 The strategy is tostart with the creation of the navbar when DOMContentLoaded event is done with a loop through all the Section selector.
 The navbar item will have a link to the section to scroll automatically when link clicked
 An event listener will be added to the navbar but getting the event.target in order to kwno which element is clicked to change the style of the navbar when clicked 
-
+The sections will be tracked by an IntersectionObserver to know when visible in the viewport
 
 
 ## Navbar
+
+The navbar is created by getting all elements by tag type defined by a global variable.
+For each section a menu item is created with a link to the section to allow automatic scroll
+
+## Observer
+
+An intersection observer is defined as global variable with a callback that will then lock if intersection is detected to toggle the active section
+
+## ClickEvent
+
+An event listener is added to the navbar in order to get the click in the menu items to also toggle the active section
--- js/app.js
+++ js/app.js
@@ -17,14 +17,54 @@
  * Define Global Variables
  * 
 */
-let navBarElement;
+//Allow to have all the names linking to the CSS stylesheet in only one place on the code
+const navBarId='navbar__list'; 
+const sectionsTagType = 'section'; 
+const classSelectorMenuItem = 'menu__link'; 
+const sectionKeyAttribute = 'data-nav'; 
+const activeMenuLinkclass = 'active_menu__link';
+const activeSectionClass = 'your-active-class';
+
+//Intersection observer to detect section on scroll
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.4
+  }
+const observer = new IntersectionObserver(intersectionObserverCallback, options);
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
+/**
+* @description Function called need to toggle the visualized sections items
+* @param {String} key - The section key of the visualized section
+*/
+function toggleActiveSection(key) {
+    toggleActiveElementType(key,sectionKeyAttribute,sectionsTagType,activeSectionClass);
+    toggleActiveElementType(key,sectionKeyAttribute,`.${classSelectorMenuItem}`,activeMenuLinkclass);
+}
 
+  /**
+* @description Function called need to toggle a type of elements by a type of attribute equal to a key for a given CSS class
+* @param {String} keyValue - The section key of the visualized section
+* @param {String} keyName - The attribute name for the key
+* @param {String} selector - The selector of type of elements
+* @param {String} activeClass - The active class on the CSS stylesheet
+*/
+function toggleActiveElementType(keyValue,keyName,selector,activeClass) {
+    const elements = document.querySelectorAll(selector);
+
+    for(element of elements){
+        if(element.getAttribute(keyName) == keyValue){
+            element.classList.add(activeClass);
+        }else{
+            element.classList.remove(activeClass)
+        }
+    }
+  }
 
 
 /**
@@ -32,14 +72,56 @@ let navBarElement;
  * Begin Main Functions
  * 
 */
+//NavBar item click delegate
+/**
+* @description Function called when there is a click in a NavBar element
+* @param {Event} event - The event associated with the event listener
+*/
+function clickNavBarItemDelegate(event) {
+  const sectionKey = event.target.getAttribute(sectionKeyAttribute);
+  toggleActiveSection(sectionKey);
+}
 
 // build the nav
+/**
+* @description Function called when the document is loaded 
+* @param {Event} event - The event associated with the event listener
+*/
+function buildMenuFunction(event) {
+    const sections = document.getElementsByTagName(sectionsTagType);
+    const fragment = document.createDocumentFragment();
+    let navBarElement = document.getElementById(navBarId);
+    for (section of sections){
+        const liNewElement = document.createElement('li');
+        const aNewElement = document.createElement('a');
+        
+        aNewElement.classList.toggle(classSelectorMenuItem);
+        aNewElement.textContent = section.getAttribute(sectionKeyAttribute);
+        aNewElement.setAttribute('href', `#${section.id}`);
+        aNewElement.setAttribute(sectionKeyAttribute,section.getAttribute(sectionKeyAttribute));
+
+        liNewElement.appendChild(aNewElement);
+        fragment.appendChild(liNewElement);
+        observer.observe(section);
+    }
+    navBarElement.appendChild(fragment);
+    navBarElement.addEventListener('click',clickNavBarItemDelegate,true);
+}
 
 
 // Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
+/**
+* @description Function called when the document is loaded 
+* @param {List IntersectionObserverEntry} entries - The list of entries found in the viewport
+* @param {IntersectionObserver} observer - The observer doing the job of tracking the sections
+*/
+function intersectionObserverCallback(entries, observer){
+    const isIntersecting = entries[0].isIntersecting;
+    if(isIntersecting){
+        const sectionKey = entries[0].target.getAttribute(sectionKeyAttribute);
+        toggleActiveSection(sectionKey);
+    }
+}
 
 
 /**
@@ -47,33 +129,7 @@ let navBarElement;
  * Begin Events
  * 
 */
-
-/**
-* @description Function called when the document is loaded 
-* @param {Event} event - The event associated with the event listener
-*/
-function buildMenuFunction(event) {
-    navBarElement = document.getElementById('navbar__list');
-    const containers = document.getElementsByClassName('landing__container');
-    const fragment = document.createDocumentFragment();
-    for (container of containers){
-        const liNewElement = document.createElement('li');
-        const aNewElement = document.createElement('a');
-        const section = container.parentElement;
-
-        aNewElement.classList.toggle('menu__link');
-        aNewElement.textContent = section.getAttribute('data-nav');
-        aNewElement.setAttribute('href', `#${section.id}`);
-
-        liNewElement.appendChild(aNewElement);
-        fragment.appendChild(liNewElement);
-    }
-    navBarElement.appendChild(fragment);
-}
-
-// Scroll to section on link click
-
 //DOMContentLoaded event
-document.addEventListener('DOMContentLoaded', buildMenuFunction(event));
+document.addEventListener('DOMContentLoaded', buildMenuFunction);
 
 
